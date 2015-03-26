f542c7c7457d181e6d6332a477b9d56401d31b00
Phil Marshall <dr.phil.marshall@gmail.com>
2015-03-26 13:55:30 -0700

    Adapted from astrohackweek!

---
+++ contact.php
@@ -0,0 +1,32 @@
+<?php
+$field_name = $_POST['cf_name'];
+$field_email = $_POST['cf_email'];
+$field_message = $_POST['cf_message'];
+
+$mail_to = 'daniela.huppenkothen@nyu.edu';
+$subject = 'Message from a site visitor '.$field_name;
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Message: '.$field_message;
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Thank you for the message. We will contact you shortly.');
+		window.location = 'contact.htm';
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Message failed. Please, send an email to daniela.huppenkothen@nyu.edu');
+		window.location = 'contact.htm';
+	</script>
+<?php
+}
+?>
